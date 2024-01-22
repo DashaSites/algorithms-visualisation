4d8faa0898d8b1ad40ef5fecb932b4f803daead9
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2024-01-22 20:02:07 +0200

    started string algorithm

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./string.module.css";
+import { Input } from "../ui/input/input";
 
 export const StringComponent: React.FC = () => {
+
+
+
+
+
+  
   return (
     <SolutionLayout title="Строка">
+      <div className={styles.inputAndButtonBox}>
+        <Input className={styles.input} />
+
+      </div>
      
     </SolutionLayout>
   );
