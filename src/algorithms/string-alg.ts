ac319656eb9c410f0325c4c837efb2f29ea59dcf
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2024-01-22 22:31:59 +0200

    wrote algorith-1 (string)

---
+++ src/algorithms/string-alg.ts
@@ -0,0 +1,23 @@
+console.log("start")
+
+const reverseString = (inputedString: string): string => {
+  // завожу массив, чтоб положить в него перевернутую строку, 
+  // тк тайпскрипт не позволяет изменять данные типа "строка"
+  const outputedStringInArray: string[] = [];
+
+  for (let i = 0; i <= inputedString.length-1; i++) {
+
+    outputedStringInArray[inputedString.length - i - 1] = inputedString[i];
+  
+  } 
+// привожу итоговый массив к строке, тк моя функция должна возвращать строку
+return outputedStringInArray.join("");
+}
+
+console.log(reverseString("1234567"))
+
+
+console.log("finish")
+
+
+
