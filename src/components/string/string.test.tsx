108dae16ade028dc2c7bc0f85ad9730dc48532ef
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2024-03-16 02:43:45 +0200

    prepared test cases for string algorithm testing

---
+++ src/components/string/string.test.tsx
@@ -0,0 +1,43 @@
+import { ElementStates } from "../../types/element-states";
+import { useOutputElements } from "./utils";
+
+
+type OutputElement = {
+  symbol: string;
+  state: ElementStates;
+};
+
+
+
+describe("Testing of a string inverting algorithm", () => {
+
+  it("Is a string with even number of symbols inverted correctly", () => {
+    // ПРИМЕРНЫЙ ПЛАН:
+    // Положить в переменную строку (любую рандомную, "kitten")
+    // В другую переменную положить перевернутую строку ("nettik")
+    // Вызвать функцию-алгоритм и положить в переменную factual результат его работы
+    // Проверить c помощью нужного метода, что expect(factual) будет соответствовать ожидаемому значению
+    // НАВЕРНО, ВСЯ ЭТА ФУНКЦИЯ ДОЛЖНА БЫТЬ АСИНХРОННОЙ
+  });
+
+
+
+
+
+  it("Is a string with odd number of symbols inverted correctly", () => { 
+
+  });
+
+
+
+  it("Is a string with one symbol inverted correctly", () => { 
+
+  });
+
+
+
+  it("Is an empty string inverted correctly", () => { 
+
+  });
+
+});
