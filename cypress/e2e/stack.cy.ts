4137ac42680415d8ddf6bf470bc072a2f1a86ebb
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2024-03-24 22:40:23 +0200

    added all e2e tests for stack page

--- cypress/e2e/stack.cy.ts
+++ cypress/e2e/stack.cy.ts
@@ -6,10 +6,12 @@ describe("Stack testing", () => {
     cy.visit(routes.stack);
   })
 
+  
   it("checks if the add button is disabled while the input is empty", () => {
     cy.get("input").should("have.value", "");
     cy.contains("Добавить").should("be.disabled");
   })
+
 
   it("checks if a new element is being added correctly and animation works on each step", () => {
 
@@ -96,7 +98,59 @@ describe("Stack testing", () => {
     cy.get("@firstElementCircle").invoke("attr", "class").then(classes => {
       expect(classes).to.contain("circle_default");
     })
-  })
+  });
 
+
+  it("checks if the clear button works correctly", () => {
+
+    const circleColoredSelector = "[class*=circle_circle]";
+    // Добавляю первый элемент
+    cy.get("input").type("1");
+    cy.contains("Добавить").click();
+    cy.get("[class*=circle_content]").first().as("firstElement");
+    cy.get("@firstElement").contains("1");
+
+    cy.get("@firstElement").find(circleColoredSelector).as("firstElementCircle");
+    cy.get("@firstElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_changing");
+    })
+    cy.wait(1000);
+    cy.get("@firstElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_default");
+    });
+
+    // Добавляю второй элемент
+    cy.get("input").type("2");
+    cy.contains("Добавить").click();
+    cy.get("[class*=circle_content]").should('have.length', 2);
+    cy.get("[class*=circle_content]").last().as("lastElement");
+    cy.get("@lastElement").find(circleColoredSelector).as("lastElementCircle");
+    cy.get("@lastElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_changing");
+    })
+    cy.wait(1000);
+    cy.get("@lastElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_default");
+    });
+
+    // Добавляю третий элемент
+    cy.get("input").type("3");
+    cy.contains("Добавить").click();
+    cy.get("[class*=circle_content]").should('have.length', 3);
+    cy.get("[class*=circle_content]").last().as("lastElement");
+    cy.get("@lastElement").contains("3");
+    cy.get("@lastElement").find(circleColoredSelector).as("lastElementCircle");
+    cy.get("@lastElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_changing");
+    })
+    cy.wait(1000);
+    cy.get("@lastElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_default");
+    });
+
+    // Очищаю стек
+    cy.contains("Очистить").click();
+    cy.get("[class*=circle_content]").should('have.length', 0);
+  });
 
 });
