fa0edb64cdcae11fa381b3bace5f77a2f2d22352
Dasha Kholobaeva <dashaholobaeva@gmail.com>
2024-03-24 22:14:27 +0200

    added third e2e test for string page

--- cypress/e2e/stack.cy.ts
+++ cypress/e2e/stack.cy.ts
@@ -50,4 +50,53 @@ describe("Stack testing", () => {
     // КАК УДАЛИТЬ: cy.get('.check-box-sub-text').should('not.exist');
   })
 
+
+  it("checks if an element is being removed correctly", () => {
+
+    const circleColoredSelector = "[class*=circle_circle]";
+    // Рендер первого элемента
+    cy.get("input").type("10").should("have.value", "10");
+    cy.contains("Добавить").click();
+
+    cy.get("[class*=circle_content]").first().as("firstElement");
+    cy.get("@firstElement").contains("10");
+
+    cy.get("@firstElement").find(circleColoredSelector).as("firstElementCircle");
+    cy.get("@firstElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_changing");
+    })
+    cy.wait(1000);
+    cy.get("@firstElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_default");
+    });
+
+    // Рендер второго элемента
+    cy.get("input").type("20");
+    cy.contains("Добавить").click();
+    cy.get("[class*=circle_content]").should('have.length', 2);
+    cy.get("[class*=circle_content]").last().as("lastElement");
+
+    /// Удаление
+    cy.contains("Удалить").should("not.be.disabled").click();
+     // Проверка, что кружок второго элемента загорелся розовым
+    cy.get("@lastElement").find(circleColoredSelector).as("lastElementCircle");
+    cy.get("@lastElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_changing");
+    });
+
+    cy.wait(1000);
+    // Проверка, что в массиве остался один элемент
+    cy.get("[class*=circle_content]").should('have.length', 1);
+    // И у него есть строковые значения "10", "top" и "0"
+    cy.get("@firstElement").contains("10");
+    cy.get("@firstElement").contains("top");
+    cy.get("@firstElement").contains("0");
+    // А его кружок - синий
+    cy.get("@firstElement").find(circleColoredSelector).as("firstElementCircle");
+    cy.get("@firstElementCircle").invoke("attr", "class").then(classes => {
+      expect(classes).to.contain("circle_default");
+    })
+  })
+
+
 });
